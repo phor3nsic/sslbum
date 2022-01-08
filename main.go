b8ee28729303a31d76015b3321aef31ce3bcd83d
Walleson Moura <phor3nsic@MacBook-Air-de-Walleson.local>
2022-01-08 10:47:15 -0300

    Dup Ips #1 resolved

--- main.go
+++ main.go
@@ -8,9 +8,21 @@ import (
 	"net/http"
 )
 
+func main() {
+	flag.BoolVar(&silent, "s", false, "Silent Mode")
+	flag.StringVar(&domain, "d", "", "Domaion to check")
+	flag.StringVar(&apiKey, "k", "", "Shodan ApiKey")
+	flag.Parse()
+	if silent != true {
+		fmt.Println(banner)
+	}
+	ssl(domain, apiKey)
+}
+
 var domain string
 var silent bool
 var apiKey string
+var results []string
 var banner = `
 
 █▀ █▀ █░░ █▄▄ █░█ █▀▄▀█
@@ -25,6 +37,16 @@ I███████████████████].
 
 [!] Run: sslbum -d example.com -k SHODANAPIKEY
 `
+
+func dupCheck(val string, list []string) bool {
+
+	for _, b := range list {
+		if b == val {
+			return false
+		}
+	}
+	return true
+}
 
 type Response struct {
 	Matches []struct {
@@ -41,39 +63,40 @@ func request(url string) {
 		data, _ := ioutil.ReadAll(resp.Body)
 		var responseObj Response
 		json.Unmarshal([]byte(string(data)), &responseObj)
+
 		if responseObj.Total == 0 {
 			return
 		}
-		concat := fmt.Sprint("[+] Total found: ", responseObj.Total)
+
 		if silent != true {
+			concat := fmt.Sprint("[+] Total found: ", responseObj.Total)
 			fmt.Println(concat)
 		}
+
 		for i := 0; i < len(responseObj.Matches); i++ {
-			fmt.Println(responseObj.Matches[i].IPStr)
+			var ip string
+			ip = responseObj.Matches[i].IPStr
+			if dupCheck(ip, results) {
+				results = append(results, ip)
+			}
+
 		}
 
 	}
 }
 
-func shodan(domain string, api string) {
-	shodanQuery := []string{`ssl:`, "ssl.cert.subject.CN:"}
+func ssl(domain string, api string) {
+	shodanQuery := []string{"ssl:", "ssl.cert.subject.CN:"}
 	for _, q := range shodanQuery {
 		shodanApi := "https://api.shodan.io/shodan/host/search?key=" + api + "&query=" + q + domain
 		if silent != true {
-			fmt.Println("[-] Searching ...")
+			fmt.Println("[-] " + q + " Searching ...")
 		}
 		request(shodanApi)
 	}
 
-}
-
-func main() {
-	flag.BoolVar(&silent, "s", false, "Silent Mode")
-	flag.StringVar(&domain, "d", "", "Domaion to check")
-	flag.StringVar(&apiKey, "k", "", "Shodan ApiKey")
-	flag.Parse()
-	if silent != true {
-		fmt.Println(banner)
+	for i := 0; i < len(results); i++ {
+		fmt.Println(results[i])
 	}
-	shodan(domain, apiKey)
+
 }
